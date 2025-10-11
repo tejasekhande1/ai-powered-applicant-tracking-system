8bc4e9206d1cf334f9a844b58d94d06cbf24a606
Tejas Ekhande <tejasekhande01@gmail.com>
2025-10-12 00:19:24 +0530

    Refactor: home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,26 +14,24 @@ function Home() {
     }, [auth.isAuthenticated]);
 
     return (
-        <>
-            <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-                <section className="main-section">
-                    <Navbar/>
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+            <section className="main-section">
+                <Navbar/>
 
-                    <div className="page-heading py-16 max-sm:py-4">
-                        <h1>Stay Ahead in Your Job Hunt</h1>
-                        <h2>Analyze, improve, and perfect your resume with smart AI feedback.</h2>
-                    </div>
+                <div className="page-heading py-16 max-sm:py-4">
+                    <h1>Stay Ahead in Your Job Hunt</h1>
+                    <h2>Analyze, improve, and perfect your resume with smart AI feedback.</h2>
+                </div>
 
-                    <div className="resumes-section">
-                        {
-                            resumes.map((resume: Resume, i: number) => (
-                                <ResumeCard key={i} resume={resume}/>
-                            ))
-                        }
-                    </div>
-                </section>
-            </main>
-        </>
+                <div className="resumes-section">
+                    {
+                        resumes.map((resume: Resume, i: number) => (
+                            <ResumeCard key={i} resume={resume}/>
+                        ))
+                    }
+                </div>
+            </section>
+        </main>
     )
 }
 
