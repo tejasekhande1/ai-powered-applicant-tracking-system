6a0e06c15fed275c71feb02a95eb221b163484c0
Tejas Ekhande <tejasekhande01@gmail.com>
2025-10-11 12:34:33 +0530

    Add some dummy resumes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import './App.css'
 import Navbar from "./components/Navbar.tsx";
+import {resumes} from "./constants";
+import ResumeCard from "./components/ResumeCard.tsx";
 
 function App() {
 
     return (
         <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-            <Navbar />
-
             <section className="main-section">
-                <div className="page-heading">
+                <Navbar/>
+
+                <div className="page-heading py-16">
                     <h1>Track Your Application & Resume Ratings</h1>
                     <h2>Review your submissions and check AI-powered feedback.</h2>
+                </div>
+
+                <div className="resumes-section">
+                    {
+                        resumes.map((resume: Resume, i: number) => (
+                            <ResumeCard key={i} resume={resume}/>
+                        ))
+                    }
                 </div>
             </section>
         </main>
